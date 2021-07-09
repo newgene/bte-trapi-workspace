721ac636b8a4884ad7125ba8cb062feb4716877b
Chunlei Wu <anewgene@yahoo.com>
2021-07-09 09:12:16 -0700

    build: :hammer: allow clone package repos with https too

--- scripts/clone_packages.sh
+++ scripts/clone_packages.sh
@@ -1,13 +1,25 @@
 current_folder=`pwd`
+remote_url=`git config --get remote.origin.url`
+echo $remote_url | grep -Eq ^https && remote_protocol="https"  || remote_protocol="git"
+if [ "$remote_protocol" = "https" ]; then
+   echo "Clone repos using https..."
+   base_url="https://github.com/"
+else
+   echo "Clone repos using git..."
+   base_url="git@github.com:"
+fi
+# example git URLs, https v.s. git
+# https://github.com/biothings/BioThings_Explorer_TRAPI.git
+# git@github.com:biothings/BioThings_Explorer_TRAPI.git
 
 set -x
-git clone "git@github.com:biothings/BioThings_Explorer_TRAPI.git" "./packages/@biothings-explorer/bte-trapi"
-git clone "git@github.com:biothings/bte_trapi_query_graph_handler.git" "./packages/@biothings-explorer/query_graph_handler"
-git clone "git@github.com:biothings/biolink-model.js.git" "./packages/biolink-model"
-git clone "git@github.com:biothings/biomedical_id_resolver.js.git" "./packages/biomedical_id_resolver"
-git clone "git@github.com:biothings/smartapi-kg.js.git" "./packages/@biothings-explorer/smartapi-kg"
-git clone "git@github.com:biothings/call-apis.js.git" "./packages/@biothings-explorer/call-apis"
-git clone "git@github.com:biothings/api-respone-transform.js.git" "./packages/@biothings-explorer/api-response-transform"
+git clone $base_url"biothings/BioThings_Explorer_TRAPI.git" "./packages/@biothings-explorer/bte-trapi"
+git clone $base_url"biothings/bte_trapi_query_graph_handler.git" "./packages/@biothings-explorer/query_graph_handler"
+git clone $base_url"biothings/biolink-model.js.git" "./packages/biolink-model"
+git clone $base_url"biothings/biomedical_id_resolver.js.git" "./packages/biomedical_id_resolver"
+git clone $base_url"biothings/smartapi-kg.js.git" "./packages/@biothings-explorer/smartapi-kg"
+git clone $base_url"biothings/call-apis.js.git" "./packages/@biothings-explorer/call-apis"
+git clone $base_url"biothings/api-respone-transform.js.git" "./packages/@biothings-explorer/api-response-transform"
 
 cd "./packages/@biothings-explorer/call-apis"
 ln -s ../../../scripts/tsconfig.json_call-apis ./tsconfig.json
